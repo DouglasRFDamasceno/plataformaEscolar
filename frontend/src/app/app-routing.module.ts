0905af65b300e233d1cf35bd8a5be9e2049c1735
Douglas <douglas.damasceno@tcsindustrial.com.br>
2020-12-25 12:23:18 -0300

    Estruturação template

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './views/home/home.component';
+import { SchoolMaterialComponent } from './views/school-material/school-material.component';
+import { TestComponent } from './views/test/test.component';
+
+const routes: Routes = [
+	{
+		path: '',
+		component: HomeComponent
+	}, {
+		path: 'schoolMaterial',
+		component: SchoolMaterialComponent
+	}, {
+		path: 'test',
+		component: TestComponent
+	}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ frontend/src/app/views/school-material/school-material.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-school-material',
+  templateUrl: './school-material.component.html',
+  styleUrls: ['./school-material.component.css']
+})
+export class SchoolMaterialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
